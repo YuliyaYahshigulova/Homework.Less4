298ee70405526b8fa7aa4ea482ce56a04eda09f1
YuliyaYahshigulova <lukyashkina_da@mail.ru>
2022-11-10 13:48:20 +0700

    add Less4.Task29

---
+++ Less4.Task29/Program.cs
@@ -0,0 +1,32 @@
+﻿// Console.Write("Введите положительное целое число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[8];
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    for (int index = 0; index < length; index++)
+    {
+        collection[index] = new Random().Next(1, 201);
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    for (int pos = 0; pos < count; pos++)
+    {
+        Console.WriteLine(col[pos]);
+    }
+}
+FillArray(array);
+PrintArray(array);
+
+
+
+
+
+
+// Напишите программу, которая задаёт массив из N элементов 
+// и выводит их на экран.
