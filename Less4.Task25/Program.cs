9db093a9617c608e81536dec3932e14f877548f9
YuliyaYahshigulova <lukyashkina_da@mail.ru>
2022-11-10 14:56:18 +0700

    add Less4.Task25

---
+++ Less4.Task25/Program.cs
@@ -0,0 +1,12 @@
+﻿int Stepen(int A, int B)
+{
+    int result = 1;
+    for (int i = 1; i <= B; i++)
+    {
+        result = result * A;
+    }
+    return result;
+}
+int pow = Stepen(2, 10);
+Console.WriteLine(pow);
+
