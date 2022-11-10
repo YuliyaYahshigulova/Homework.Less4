d35266dc82c8655392baf96974d016a1fcdc18cd
YuliyaYahshigulova <lukyashkina_da@mail.ru>
2022-11-10 16:35:54 +0700

    add Less4.Task27

---
+++ Less4.Task27/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int MethodSum(int N)
+{
+    int s = 0;
+    while (N > 0)
+    {
+        s = s + N % 10;
+        N = N / 10;
+    }
+    Console.WriteLine(s);
+    return N;
+}
+
+N = MethodSum(N);
